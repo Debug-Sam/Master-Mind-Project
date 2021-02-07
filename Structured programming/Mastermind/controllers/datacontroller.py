fbde4a902fe57428df9d6fa86874f9e29833eedb
Debug-Sam <74404094+Debug-Sam@users.noreply.github.com>
2021-02-07 17:33:17 +0100

    De basis van de game gemaakt, moet alleen nog de witte pinnen in orde krijgen

--- Structured programming/Mastermind/controllers/datacontroller.py
+++ Structured programming/Mastermind/controllers/datacontroller.py
@@ -1,21 +1,43 @@
-
-
+from itertools import permutations
+from itertools import combinations_with_replacement
+import random
 
 
 class DataController():
 
     @staticmethod
-    def data_list():
-        lst = ["A", "B", "C", "D", "E", "F" ]
-        for i in enumerate(lst):
-            print(i)
-
-    @staticmethod
     def random_code():
-        return
+        combinations = combinations_with_replacement(["A", "B", "C", "D", "E", "F"], 4)
+        lst = []
+        for i in list(combinations):
+            lst.append(i)
+        random_seq = random.choice(lst)
+        string = ""
+        for j in random_seq:
+            string += j
+        return string
 
     @staticmethod
-    def feedback():
-        return
+    def feedback(guess, code):
+
+        if guess == code:
+            pass
+        goodindex = 0
+        good = 0
+
+        for i in range(len(code)):
+            if guess[i] == code[i]:
+                goodindex += 1
+                guess.replace(guess[i], "")
+
+        for j in guess:
+            if j in code:
+                good += 1
+
+        feedback = goodindex, good
+        return feedback
+
+
+
 
 
--- Structured programming/Mastermind/views/menu.py
+++ Structured programming/Mastermind/views/menu.py
@@ -1,12 +1,60 @@
-
+from controllers.datacontroller import DataController as DC
 
 
 class Menu():
 
-    @staticmethod
-    def menu():
+    def __init__(self):
+        pass
+
+    def menu_codebreaker(self, code):
+
+        guess = input("What is your guess: ")
+
+        if guess == code:
+            print("Congratulations you've cracked the code!")
+        print(DC.feedback(guess= guess, code= code))
+
+
+
+    def menu_codemaker(self):
         return
+
+    def menu(self):
+        string = """
+        1: Play as CodeMaker
+        2: Play as CodeBreaker
+        """
+        amount = 9
+        print(string)
+        option = input("Your choice: ")
+        if option == "1":
+            print("Sorry we don't have that function yet")
+        elif option == "2":
+            print("The computer has thought of a code")
+            random_code = DC.random_code()
+            while True:
+                amount += -1
+                print(amount)
+                if amount == 0:
+                    break
+                print(random_code)
+                self.menu_codebreaker(random_code)
 
     @staticmethod
     def introduction():
-        return
+        string = """    
+        Welcome challanger.... In our game of MasterMind
+        
+        Gamerules: 
+        The computer chooses a random code existing of A, B, C, D, E or F. 
+        for example: AACD
+        It is your job to crack the code. by using a max of 8 guesses if you haven't guessed the code untill then you've lost the game.
+        The computer will give you a feedback existing of (A, B) 
+        A stands for the amount of letters good and on the good spot.
+        B stands for the amount of letters good but not on the good spot.
+                        
+        We wish you goodluck! you'll need it!
+        """
+        return string
+
+
